f2132cf6b9a3582f36a2f6f38b2e195b604bddba
Mate Kamaras <103876505+Murci2022@users.noreply.github.com>
2022-09-23 16:26:30 +0200

    add edit botton and more inputs

--- src/pages/createInvoice.jsx
+++ src/pages/createInvoice.jsx
@@ -2,7 +2,11 @@ import {useState} from 'react';
 
 export default function CreateInvoice() {
 	const [invoice, setInvoice] = useState(false);
+	/* -------------------USER DATA ---------------------*/
 	const [name, setName] = useState('');
+	const [street, setStreet] = useState('');
+	const [city, setCity] = useState('');
+	const [taxID, setTaxID] = useState('');
 	const [amount, setAmount] = useState('');
 
 	return (
@@ -12,12 +16,22 @@ export default function CreateInvoice() {
 					<h1>Your Invoice</h1>
 					<ul>
 						<li>{name}</li>
-						<li>{amount}</li>
+						<li>{street}</li>
+						<li>{city}</li>
+						<li>{taxID}</li>
+						<div>
+							<li>
+								<h1>{amount}</h1>
+							</li>
+						</div>
 					</ul>
+					<button onClick={() => setInvoice(false)}>Edit</button>
 				</div>
 			) : (
 				<form>
 					<h1>Create Invoice</h1>
+
+					{/* --------------Freelance Data-------------- */}
 					<label htmlFor="name">Name</label>
 					<input
 						type="text"
@@ -26,6 +40,33 @@ export default function CreateInvoice() {
 						placeholder="e.g. John Dylen"
 						value={name}
 						onChange={() => setName(event.target.value)}
+					/>
+					<label htmlFor="street">Street</label>
+					<input
+						type="text"
+						id="street"
+						name="street"
+						placeholder="e.g. Som Street"
+						value={street}
+						onChange={() => setStreet(event.target.value)}
+					/>
+					<label htmlFor="name">City</label>
+					<input
+						type="text"
+						id="city"
+						name="city"
+						placeholder="e.g. Hamburg"
+						value={city}
+						onChange={() => setCity(event.target.value)}
+					/>
+					<label htmlFor="name">Tax ID</label>
+					<input
+						type="text"
+						id="taxID"
+						name="taxID"
+						placeholder="e.g. 34567890890"
+						value={taxID}
+						onChange={() => setTaxID(event.target.value)}
 					/>
 					<label htmlFor="amount">amount</label>
 					<input
