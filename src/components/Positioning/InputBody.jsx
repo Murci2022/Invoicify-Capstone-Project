998c4059eaeefab52809fb10c016119d042afe28
Mate Kamaras <103876505+Murci2022@users.noreply.github.com>
2022-10-16 15:39:47 +0200

    correction for PR

--- src/components/Positioning/InputBody.jsx
+++ src/components/Positioning/InputBody.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 const InputBody = styled.div`
-	/* background-color: rgba(20, 26, 41, 1); */
+	background-color: rgba(20, 26, 41, 1);
 `;
 
 export default InputBody;
